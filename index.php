76f80b732e7b1fea4db3f87eedb4965ee15f804c
unknown <ihor.butenko08@gmail.com>
2022-11-29 20:49:18 +0200

    replace widget to components

--- index.php
+++ index.php
@@ -79,10 +79,7 @@
                 <?php include "./components/categories.php"?>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php include "./components/widget.php"?>
 
             </div>
 
